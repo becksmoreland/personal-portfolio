f46be5869e9900da8ee743b3316b4e0c7dd1431b
bmorelandnasa <bmorelandnasa@gmail.com>
2026-09-17 17:15:55 -0700

    Remove Sites deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://sdsu-aerospace-portfolio.humble-ball-4570.chatgpt.site',
   build: {
     format: 'directory'
   }
